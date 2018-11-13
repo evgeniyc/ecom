59b34b1602f5e2a66571838a019b169b7fa7ede6
evgeniyc <evgeniychp@i.ua>
2018-11-13 17:06:27 +0200

    tuesday at work

--- application/views/items/index1.php
+++ application/views/items/index1.php
@@ -2,8 +2,8 @@
 	<h3>Все модели производителя <?php echo $cat; ?></h3>
 	<section class="row no-gutters justify-content-center">
 		<?php foreach ($items as $item): ?>
-			<div class="product-wrapper">
-				<div class="product">
+			<div id="items-row" class="col-sm-6 col-md-4 col-lg-3">
+				<div id="item">
 					<a href="../item/view">
 						<div id="item-title"><?php echo $item->model;?></div>
 						<div id="item-img"><img src="<?php echo base_url()."assets/img/items/".$item->img; ?>" alt=""></div>
